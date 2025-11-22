16c69aea889e13677d47a21e894c344393c3d200
1eehvunzin <hyunjinlee@ewha.ac.kr>
2025-11-22 23:13:19 +0900

    feat: 홈 화면 구현 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,12 @@ import "../style.css";
 import { useState } from "react";
 import Search from "../assets/Search.png";
 import Coursecard from "../components/Coursecard";
+import Profile from "../assets/profile.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const [user, setuser] = useState("김리얼");
+  const navigate = useNavigate();
 
   return (
     <div className="page">
@@ -19,11 +22,38 @@ export default function Home() {
           <h4 style={{ color: "#7D8A95" }}>환영합니다!</h4>
           <h3>{user} 님</h3>
         </div>
-        <button>
-          <img src={Search} alt="검색" />
-        </button>
+        <div>
+          <button>
+            <img src={Search} alt="검색" />
+          </button>
+          <button onClick={() => navigate("/profile")}>
+            <img
+              src={Profile}
+              alt="프로필 수정"
+              style={{ width: "28px", height: "28px" }}
+            />
+          </button>
+        </div>
+      </div>
+      <h2 style={{ margin: "24px 0 12px 0" }}>이번 학기, 선택과 집중 리포트</h2>
+      <div
+        style={{
+          backgroundColor: "#FAFAFC",
+          border: "1px solid #F4F4F6",
+          color: "#000000",
+          padding: "16px",
+          display: "flex",
+          borderRadius: "16px",
+          width: "90%",
+          margin: "12px 0px",
+        }}
+      >
+        예시 텍스트 입니다
       </div>
       <h2 style={{ margin: "24px 0 12px 0" }}>수강 관리 중인 과목</h2>
+      <Coursecard />
+      <Coursecard />
+      <Coursecard />
       <Coursecard />
       <Coursecard />
       <Coursecard />
