c73b08d91465a6bde50124c60ded463117a112b9
1eehvunzin <hyunjinlee@ewha.ac.kr>
2025-11-22 21:44:09 +0900

    feat: Home 페이지 레이아웃 구현

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import "../style.css";
+import { useState } from "react";
+import Search from "../assets/Search.png";
+
+export default function Home() {
+  const [user, setuser] = useState("김리얼");
+
+  return (
+    <div className="page">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <div>
+          <h4 style={{ color: "#7D8A95" }}>환영합니다!</h4>
+          <h3>{user} 님</h3>
+        </div>
+        <button>
+          <img src={Search} alt="검색" />
+        </button>
+      </div>
+      <h2 style={{ margin: "24px 0px" }}>수강 관리 중인 과목</h2>
+    </div>
+  );
+}
